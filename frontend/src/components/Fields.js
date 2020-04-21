9405f5133f1e0bc3f26472e8103c9b665ccc1dc8
svrsaljko <vrsaljko tjepan@gmail.com>
2020-04-21 12:35:34 +0200

    Add react hooks to Fileds componnet

--- frontend/src/components/Fields.js
+++ frontend/src/components/Fields.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { setAllFields } from '../actions/actions';
 import {
@@ -11,162 +11,164 @@ import {
 import Walls from './Walls';
 import { uuid } from 'uuidv4';
 
-class Fields extends Component {
-  state = { fields: [] };
-
-  componentDidMount() {
-    let fields = [];
-    let fieldId = 0;
-    let topWall = false;
-    let bottomWall = false;
-    let rightWall = false;
-    let leftWall = false;
-    //OKVIR
-    for (let i = 0; i < NUMBER_OF_ROWS; i++) {
-      for (let j = 0; j < NUMBER_OF_COLUMNS; j++) {
-        if (i === 0) {
-          topWall = true;
+const initializeLevel = () => {
+  let fields = [];
+  let fieldId = 0;
+  let topWall = false;
+  let bottomWall = false;
+  let rightWall = false;
+  let leftWall = false;
+  //OKVIR
+  for (let i = 0; i < NUMBER_OF_ROWS; i++) {
+    for (let j = 0; j < NUMBER_OF_COLUMNS; j++) {
+      if (i === 0) {
+        topWall = true;
+      }
+      if (i === NUMBER_OF_ROWS - 1) {
+        bottomWall = true;
+      }
+      if (j === 0) {
+        leftWall = true;
+      }
+      if (j === NUMBER_OF_COLUMNS - 1) {
+        rightWall = true;
+      }
+      //
+      //TESTNI LEVEL
+      if (j === 2) {
+        if (i === 7) {
+          bottomWall = true;
+          leftWall = true;
         }
-        if (i === NUMBER_OF_ROWS - 1) {
+        if (i > 3 && i < 7 && i !== 5) {
+          leftWall = true;
+        }
+        if (i === 3) {
+          topWall = true;
+          leftWall = true;
+        }
+        if (i === 5) {
           bottomWall = true;
         }
-        if (j === 0) {
+      }
+      if (j === 4) {
+        if (i === 2) {
+          topWall = true;
           leftWall = true;
         }
-        if (j === NUMBER_OF_COLUMNS - 1) {
+        if (i === 8) {
+          bottomWall = true;
           rightWall = true;
         }
-        //
-        //TESTNI LEVEL
-        if (j === 2) {
-          if (i === 7) {
-            bottomWall = true;
-            leftWall = true;
-          }
-          if (i > 3 && i < 7 && i !== 5) {
-            leftWall = true;
-          }
-          if (i === 3) {
-            topWall = true;
-            leftWall = true;
-          }
-          if (i === 5) {
-            bottomWall = true;
-          }
-        }
-        if (j === 4) {
-          if (i === 2) {
-            topWall = true;
-            leftWall = true;
-          }
-          if (i === 8) {
-            bottomWall = true;
-            rightWall = true;
-          }
-        }
-        if (j === 6) {
-          if (i === 7) {
-            bottomWall = true;
-            rightWall = true;
-          }
-          if (i > 3 && i < 7 && i !== 5) {
-            rightWall = true;
-          }
-          if (i === 3) {
-            topWall = true;
-            rightWall = true;
-          }
-          if (i === 5) {
-            topWall = true;
-          }
-        }
-
-        if (j === 3 || j === 5) {
-          if (i === 3) {
-            topWall = true;
-            bottomWall = true;
-          }
-          if (i === 7) {
-            topWall = true;
-            bottomWall = true;
-          }
-        }
-        if ((i === 5 || i === 4 || i === 6) && j === 4) {
-          leftWall = true;
+      }
+      if (j === 6) {
+        if (i === 7) {
+          bottomWall = true;
           rightWall = true;
         }
-        if (i === 5 && (j === 3 || j === 5)) {
-          leftWall = true;
+        if (i > 3 && i < 7 && i !== 5) {
           rightWall = true;
         }
+        if (i === 3) {
+          topWall = true;
+          rightWall = true;
+        }
+        if (i === 5) {
+          topWall = true;
+        }
+      }
 
-        //
+      if (j === 3 || j === 5) {
+        if (i === 3) {
+          topWall = true;
+          bottomWall = true;
+        }
+        if (i === 7) {
+          topWall = true;
+          bottomWall = true;
+        }
+      }
+      if ((i === 5 || i === 4 || i === 6) && j === 4) {
+        leftWall = true;
+        rightWall = true;
+      }
+      if (i === 5 && (j === 3 || j === 5)) {
+        leftWall = true;
+        rightWall = true;
+      }
 
-        fields.push({
-          top: FIELD_HEIGHT * i,
-          left: FIELD_WIDTH * j,
+      //
+
+      fields.push({
+        top: FIELD_HEIGHT * i,
+        left: FIELD_WIDTH * j,
+        topWall,
+        bottomWall,
+        rightWall,
+        leftWall,
+        fieldId,
+        leftFieldId: j === 0 ? null : fieldId - 1,
+        rightFieldId: j === NUMBER_OF_COLUMNS - 1 ? null : fieldId + 1,
+        topFieldId: i === 0 ? null : fieldId - NUMBER_OF_COLUMNS,
+        bottomFieldId:
+          i === NUMBER_OF_ROWS - 1 ? null : fieldId + NUMBER_OF_COLUMNS,
+      });
+
+      fieldId++;
+      topWall = false;
+      bottomWall = false;
+      rightWall = false;
+      leftWall = false;
+    }
+  }
+  return fields;
+};
+
+function Fields(props) {
+  const [fields, setFields] = useState(() => {
+    return initializeLevel();
+  });
+
+  useEffect(() => {
+    setFields(fields);
+    props.setAllFields(fields);
+  }, [fields, props]);
+
+  return (
+    <div>
+      {fields.map((field) => {
+        let {
+          top,
+          left,
+          //fieldId,
           topWall,
           bottomWall,
           rightWall,
           leftWall,
-          fieldId,
-          leftFieldId: j === 0 ? null : fieldId - 1,
-          rightFieldId: j === NUMBER_OF_COLUMNS - 1 ? null : fieldId + 1,
-          topFieldId: i === 0 ? null : fieldId - NUMBER_OF_COLUMNS,
-          bottomFieldId:
-            i === NUMBER_OF_ROWS - 1 ? null : fieldId + NUMBER_OF_COLUMNS,
-        });
-        fieldId++;
-        topWall = false;
-        bottomWall = false;
-        rightWall = false;
-        leftWall = false;
-      }
-    }
-
-    this.props.setAllFields(fields);
-    this.setState({ fields });
-  }
-
-  render() {
-    console.log('fields');
-    let { fields } = this.state;
-    return (
-      <div>
-        {fields.map((field) => {
-          let {
-            top,
-            left,
-            //fieldId,
-            topWall,
-            bottomWall,
-            rightWall,
-            leftWall,
-          } = field;
-          return (
-            <div
-              key={uuid()}
-              style={{
-                border: '0.3px solid red',
-                width: `${FIELD_WIDTH}px`,
-                height: `${FIELD_HEIGHT}px`,
-                top: `${top}px`,
-                left: `${left}px`,
-                position: 'absolute',
-              }}
-            >
-              <Walls
-                topWall={topWall}
-                bottomWall={bottomWall}
-                rightWall={rightWall}
-                leftWall={leftWall}
-              />
-            </div>
-          );
-        })}
-      </div>
-    );
-  }
+        } = field;
+        return (
+          <div
+            key={uuid()}
+            style={{
+              border: '0.3px solid red',
+              width: `${FIELD_WIDTH}px`,
+              height: `${FIELD_HEIGHT}px`,
+              top: `${top}px`,
+              left: `${left}px`,
+              position: 'absolute',
+            }}
+          >
+            <Walls
+              topWall={topWall}
+              bottomWall={bottomWall}
+              rightWall={rightWall}
+              leftWall={leftWall}
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 const mapDispatchToProps = (dispatch) => {
