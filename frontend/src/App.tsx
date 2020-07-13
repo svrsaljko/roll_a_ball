172209e05746cc1d6af791e053aeeb2f178844bf
svrsaljko <vrsaljko tjepan@gmail.com>
2020-07-14 01:04:09 +0200

    add react-routin, signin, and signup components as pages and routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
+
 import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 function App() {
   //console.log('Mobile device to pixel ratio: ', window.devicePixelRatio);
   //console.log('Mobile device outer width: ', window.outerWidth);
   //console.log('Mobile device outer height: ', window.outerHeight);
   return (
-    <div>
-      <Home />
-    </div>
+    <Router>
+      <Route exact path="/" component={Home} />
+      <Route path="/signin" component={SignIn} />
+      <Route path="/signup" component={SignUp} />
+    </Router>
   );
 }
 
