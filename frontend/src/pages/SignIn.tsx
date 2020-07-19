e8800b20f9221bf08f1dc9d24718c9d86258b3f9
svrsaljko <vrsaljko tjepan@gmail.com>
2020-07-19 15:18:10 +0200

    add css/SignIn.css,custom hooks in hooks/ , update SignIn page

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -1,10 +1,41 @@
 import React from 'react';
 import Header from '../components/Header';
+import { useForm } from '../hooks/useForm';
+import '../css/SignIn.css';
+
 export default function SignIn() {
+  const [values, handleChange] = useForm({ email: '', password: '' });
+  const onSubmit = () => {
+    console.log('on submit', values);
+  };
   return (
     <div>
       <Header />
-      sign in
+      <div className="signin-container">
+        <div className="signin-form">
+          <input
+            name="email"
+            placeholder="Enter your email here... "
+            type="text"
+            value={values.email}
+            onChange={handleChange}
+          />
+          <input
+            name="password"
+            placeholder="Enter your password here... "
+            type="text"
+            value={values.password}
+            onChange={handleChange}
+          />
+          <button
+            onClick={() => {
+              onSubmit();
+            }}
+          >
+            SIGN IN
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
